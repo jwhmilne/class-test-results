180746d6d66acb6188c1d16ec1bc55f8d5656f21
Joe Milne <jwhmilne@gmail.com>
2022-12-05 11:24:56 +0000

    Add full stops; change -html to .html

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ const samPoints = 84;
 const alexPoints = 75;
 const jessiePoints = 99;
 const didAlexPass = !false;
-const samPointsDisplay = `Sam earned ${samPoints} points out of ${maxPoints}`;
-const alexPointsDisplay =
-`Alex earned ${alexPoints} points out of ${maxPoints}`;
-const jessiePointsDisplay =
-  `Jessie earned ${jessiePoints} points out of ${maxPoints}`;
+const samPointsDisplay = `Sam earned ${samPoints} points out of ${maxPoints}.`;
+const alexPointsDisplay = `Alex earned ${alexPoints} points out of ${maxPoints}.`;
+const jessiePointsDisplay = `Jessie earned ${jessiePoints} points out of ${maxPoints}.`;
 console.log(samPointsDisplay);
 console.log(alexPointsDisplay);
 console.log(jessiePointsDisplay);
 const samPercentage = (samPoints / maxPoints) * 100;
-const samPercentDisplay = `Sam scored ${samPercentage}%.`
+const samPercentDisplay = `Sam scored ${samPercentage}%.`;
 console.log(samPercentDisplay);
 const alexPercentage = (alexPoints / maxPoints) * 100;
-const alexPercentDisplay = `Alex scored ${alexPercentage}%.`
+const alexPercentDisplay = `Alex scored ${alexPercentage}%.`;
 console.log(alexPercentDisplay);
 const jessiePercentage = (jessiePoints / maxPoints) * 100;
-const jessiePercentDisplay = `Jessie scored ${jessiePercentage}%.`
+const jessiePercentDisplay = `Jessie scored ${jessiePercentage}%.`;
 console.log(jessiePercentDisplay);
 const samMissedPoints = maxPoints - samPoints;
 const samMissedPointsDisplay = `Sam missed ${samMissedPoints} point(s).`;
@@ -28,16 +26,15 @@ const alexMissedPoints = maxPoints - alexPoints;
 const alexMissedPointsDisplay = `Alex missed ${alexMissedPoints} point(s).`;
 console.log(alexMissedPointsDisplay);
 const jessieMissedPoints = maxPoints - jessiePoints;
-const jessieMissedPointsDisplay =
-  `Jessie missed ${jessieMissedPoints} point(s).`;
+const jessieMissedPointsDisplay = `Jessie missed ${jessieMissedPoints} point(s).`;
 console.log(jessieMissedPointsDisplay);
 const classAveragePoints = (samPoints + alexPoints + jessiePoints) / 3;
 const classAveragePercent = (classAveragePoints / maxPoints) * 100;
-const classAvgDisplay =
-  `The class average was ${classAveragePoints} points or ${classAveragePercent.toFixed(2)}%.`;
+const classAvgDisplay = `The class average was ${classAveragePoints} points or ${classAveragePercent.toFixed(
+  2
+)}%.`;
 console.log(classAvgDisplay);
-const alexPointsUpdated =
-  `Alex failed initially but did extra credit. Their new score is ${alexPoints} out of ${maxPoints}`;
+const alexPointsUpdated = `Alex failed initially but did extra credit. Their new score is ${alexPoints} out of ${maxPoints}.`;
 console.log(alexPointsUpdated);
 const listItemArray = document.querySelectorAll("li");
 listItemArray[0].innerHTML = "Raw Points:";
